c2d9d4b11a0459519ddf5e21b0b1d876090b8949
vanelnw <vanelnwaba@gmail.com>
2022-11-04 11:55:12 +0100

    clean completed task

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import Todos from './todos.js';
 
 const list = document.getElementById('list');
 const input = document.getElementById('input');
+const clearCompleteTodo = document.querySelector('.clear_completed_todo');
+const clearAll = document.querySelector('.clear');
 
 const MyTodos = new Todos();
 
@@ -11,6 +13,21 @@ let id = MyTodos.TodoList.length;
 window.onload = () => {
   MyTodos.renderTodos();
 };
+
+clearAll.addEventListener('click', () => {
+  MyTodos.TodoList = [];
+  list.replaceChildren('');
+  localStorage.setItem('todos', JSON.stringify(MyTodos.TodoList));
+});
+
+clearCompleteTodo.addEventListener('click', () => {
+  MyTodos.TodoList = MyTodos.TodoList.filter(
+    (todo) => todo.completed === false,
+  );
+  list.replaceChildren('');
+  MyTodos.renderTodos();
+  localStorage.setItem('todos', JSON.stringify(MyTodos.TodoList));
+});
 
 input.addEventListener('keydown', (event) => {
   if (event.code === 'Enter') {
